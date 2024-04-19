b9b0be657e5d402c21eb18b2c7d073a9fb33b159
subrajith05 <143308630+subrajith05@users.noreply.github.com>
2024-04-19 14:20:03 +0530

    Update circular_list.cpp

--- circular_list.cpp
+++ circular_list.cpp
@@ -18,4 +18,24 @@ class List
         {
             head=NULL;
         };
+        void display();
 };
+
+void List::display()
+{
+    if(head==NULL)
+    {
+        printf("\nThe list is empty\n");
+    }
+    else
+    {
+        struct node* temp=head;
+        printf("\nDisplaying the list\n");
+        while(temp->next!=head)
+        {
+            printf("%d\t",temp->data);
+            temp=temp->next;
+        }
+        printf("%d\n",temp->data);
+    }
+}
