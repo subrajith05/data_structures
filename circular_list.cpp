e9dc91f6bbf2070c2d664cb7f45fd63973a7fd88
Randumbcoder <161105486+VarunUdayakumar@users.noreply.github.com>
2024-04-19 22:47:48 +0530

    Update circular_list.cpp
    I did some major changes to the menu and did other minor changes. I have the backup of the original code, I have rewritten the old one.

--- circular_list.cpp
+++ circular_list.cpp
@@ -22,54 +22,73 @@ class List
     };
     
     int insert(int);
+    int del(int);
     void display();
     int size();
     void moveHeadForward();
     void moveHeadBack();
     void displayHead();
     int isEmpty();
+    ~List()
+    {
+
+    };
 };
 
 int main()
 {
     int choice,num,pos,val;
     List l1;
-    
     while(1)
-    { 
-        printf("\nEnter \n1. Insertion at the end\n2. Display\n3.Size");
+    {
+        printf("\nMENU:");
+        printf("\nEnter \n1. Insertion\n2. Display\n3. Size\n4. Deletion \n5. Exit\nEnter your choice: ");
         scanf("%d",&choice);
         switch (choice)
         {
             case 1://insertion at the end
-            {
                 printf("\nEnter the number to insert: ");
                 scanf("%d", &num);
-    
                 if (l1.insert(num))
                 {
-                    printf("%d is sucessfully inserted. ",num);
+                    printf("\n%d is sucessfully inserted. ",num);
                 }
                 else
                 { 
                     printf("\nFailed to insert.");
                 }
                 break;
-            }
 
             case 2://display
-            {
                 l1.display();
                 break;
-            }
 
-            case 3: //displaysize
-            {
+            case 3://displaysize
                 printf("\nThe size of the list: %d\n", l1.size());
                 break;
-            }
-            
-            default:
+
+            case 4://deletion of an element
+                if(l1.isEmpty())
+                {
+                    printf("deletion not possible, List is empty");
+                    break;
+                }
+                printf("\nEnter the number to delete: ");
+                scanf("%d", &num);
+                if(l1.del(num))
+                {
+                    printf("\n%d is deleted successfully",num);
+                }
+                else
+                {
+                    printf("\nFailed to delete");
+                }
+                break;
+
+            case 5:
+                exit(0);
+
+            default://For invalid choice
             printf("\n<--Enter a valid choice!-->\n");
             break;
         }  
@@ -86,9 +105,8 @@ int List::insert(int num)
     struct node *newnode=(struct node*)malloc(sizeof(struct node));
     struct node* temp=head;
     newnode->data=num;
-
     //if empty
-    if(head==NULL)
+    if(isEmpty())
     {
         head=newnode;
         newnode->next=newnode;
@@ -109,7 +127,51 @@ int List::insert(int num)
         cur+=1;
         return 1;
     }
+}
 
+//Method to delete a specific element in the doubly linked list
+//Timce complexity-O(n)
+int List::del(int num)
+{
+    if(isEmpty())
+    {
+        return 0;
+    }
+    else
+    {
+        struct node* temp=head;
+        if(temp->next==temp)
+        {
+            if(temp->data==num)
+            {
+                head=NULL;
+                cur--;
+                free(temp);
+                return 1;
+            }
+            return 0;
+        }
+        else
+        {
+            for(int i=0;i<cur;i++)
+            {
+                if(temp->data==num)
+                {
+                    if(temp==head)
+                    {
+                        head=temp->next;
+                    }
+                    temp->prev->next=temp->next;
+                    temp->next->prev=temp->prev;
+                    cur--;
+                    free(temp);
+                    return 1;
+                }
+                temp=temp->next;
+            }
+            return 0;
+        }
+    }
 }
 
 //Method to return the size of the doubly linked list
@@ -123,11 +185,10 @@ int List::size()
 //Time complexity-O(1)
 void List::moveHeadForward()
 {
-    if(head==NULL)
+    if(isEmpty())
     {
         printf("\nList is empty.");
     }
-
     else
     {
         struct node* temp=head;
@@ -140,11 +201,10 @@ void List::moveHeadForward()
 //Time complexity-O(1)
 void List::moveHeadBack()
 {
-    if(head==NULL)
+    if(isEmpty())
     {
         printf("\nList is empty.");
     }
-
     else
     {
         struct node* temp=head;
@@ -157,11 +217,10 @@ void List::moveHeadBack()
 //Time complexity-O(1)
 void List::displayHead()
 {
-    if(head==NULL)
+    if(isEmpty())
     {
         printf("\nThe list is empty.");
     }
-
     else
     {
         printf("\nHead: %d", head->data);
@@ -172,17 +231,15 @@ void List::displayHead()
 //Time complexity-O(n)
 void List::display()
 {
-    if(head==NULL)
+    if(isEmpty())
     {
-        printf("\nThe list is empty\n");
+        printf("\nThe list is empty");
     }
 
     else
     {
         struct node* temp=head;
-
         printf("\n<-The elements of the list->\n");
-
         while(temp->next!=head)
         {
             printf("%d\t",temp->data);
@@ -199,10 +256,10 @@ int List::isEmpty()
 {
     if(head==NULL)
     {
-        return 0;
+        return 1;
     }
     else
     {
-        return 1;
+        return 0;
     }
 }
