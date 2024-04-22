12f0e71cec05beddf180534125446458e94a21d4
Randumbcoder <161105486+VarunUdayakumar@users.noreply.github.com>
2024-04-22 11:12:03 +0530

    Update kinetic_heap.cpp

--- kinetic_heap.cpp
+++ kinetic_heap.cpp
@@ -1,4 +1,24 @@
 //implementation of kinetic heap
 
 #include <stdio.h>
+#include <stdlib.h>
+#define size 
 
+class KHeap
+{
+  int kheap[size];
+  public:
+    KHeap()
+    {
+        
+    };
+    int ins(int,int);
+    int  del();
+    int search(int);
+    void display(void);
+    int isEmpty(void);
+    ~KHeap()
+    {
+      
+    };
+}
